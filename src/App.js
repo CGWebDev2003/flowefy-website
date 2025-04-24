d03e31a2fa47d04dca17b3263cca8bdaf9977632
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-04-25 01:10:05 +0200

    Added Hero Section and Placeholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from './components/header/Header';
+import PlaceholderSection from './components/placeholder/Placeholder';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <PlaceholderSection></PlaceholderSection>
+      <div className='legalLinks'>
+        <a href="#">Impressum</a> <span className='devider'>|</span> <a href="#"> Datenschutz</a>
+      </div>
     </div>
   );
 }
---
+++ src/components/placeholder/Placeholder.jsx
@@ -0,0 +1,58 @@
+import "./placeholder.css";
+
+function PlaceholderSection() {
+    function openContactForm() {
+        const contactForm = document.getElementById("contactContainer");
+        contactForm.style.display = "flex";
+    }
+
+    return(
+        <div className="placeholderSection">
+            <div className="leftSide">
+                <span className="heroSubtitle">
+                    creative. individual. easy.
+                </span>
+                <h1 className="heroTitle">
+                    Marketing Made Easy
+                </h1>
+                <p className="heroText">
+                    Leider befindet sich die Website aktuell noch im Bau. Ihre Nachrichten erreichen uns natürlich trotzdem. Besuchen Sie doch unseren Instagram Profil oder senden Sie uns eine Nachricht.
+                </p>
+                <button className="ctaButton" onClick={openContactForm}>
+                    Nachricht senden
+                </button>
+            </div>
+            <div className="rightSide">
+                <div className="contactContainer" id="contactContainer">
+                    <div className="contactContent">
+                        <h1 className="contactTitle">Kontakt</h1>
+                        <div className="inputBox">
+                            <span>Vollständiger Name*</span>
+                            <input id="" placeholder="Max Mustermann" required></input>
+                        </div>
+                        <div className="inputBox">
+                            <span>Email*</span>
+                            <input id="" placeholder="beispiel@email.de" required></input>
+                        </div>
+                        <div className="inputBox">
+                            <span>Telefon*</span>
+                            <input id="" placeholder="+49 123 456789" required></input>
+                        </div>
+                        <div className="inputBox">
+                            <span>Nachricht*</span>
+                            <input id="" placeholder="Ihre Nachricht..." required></input>
+                        </div>
+                        <span className="privacyText">
+                            Mit dem senden akzeptieren Sie den <a className="privacyLink" href="#">Datenschutz</a>
+                        </span>
+                        <button type="button" className="sendButton" id="sendButton">
+                            Senden                            <i class="bi bi-send-fill"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PlaceholderSection;
