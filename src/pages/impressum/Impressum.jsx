22cd0f4b20855299bab5bf1a3d3f59de23ad9d20
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-05-13 02:01:53 +0200

    added impressum page

--- src/pages/impressum/Impressum.jsx
+++ src/pages/impressum/Impressum.jsx
@@ -6,6 +6,28 @@ function Impressum() {
     return (
         <>
             <Header />
+            <div className="mainContainerImpressum">
+                <div className="impressumBox">
+                    <h1 className="impressumTitle">Impressum</h1>
+                    <h2 className="headline">Angaben gemäß § 5 TMG:</h2>
+                    <h3 className="impressumSubTitle">flowefy. - Eine Marke von Colin Grahm Media & Commerce</h3>
+                    <p className="impressumDescription">
+                        Schwedenstraße 29A<br/>
+                        04420 Markranstädt<br/>
+                        Deutschland<br/>
+                        <br/>
+                        <strong>Vertreten durch:</strong><br/>
+                        <strong>Colin Grahm (Geschäftsführer)</strong><br/>
+                        <br/>
+                        Tel: <a className="link" href="tel:017624693418">0177-4821477</a><br/>
+                        E-Mail: <a className="link" href="mailto:colingrahmmedia@gmail.com">colingrahmmedia@gmail.com</a><br/>
+                    </p>
+                    <a href="/" className="backButton">
+                        <i className="bi bi-arrow-left"></i>
+                        Zurück
+                    </a>
+                </div>
+            </div>
             <Footer />
         </>
     );
