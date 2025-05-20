93d1fdc9018cf0aaed00e5afac93939b4aed7710
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-05-20 21:22:42 +0200

    Made Service Section responsive

--- src/pages/home/sections/serviceSection/ServiceSection.jsx
+++ src/pages/home/sections/serviceSection/ServiceSection.jsx
@@ -1,4 +1,6 @@
 import "./serviceSection.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 function ServiceSection() {
     return(
@@ -39,6 +41,59 @@ function ServiceSection() {
                     </a>
                 </div>
             </div>
+            <div className="serviceContentMobile">
+                <Swiper
+                    spaceBetween={20}
+                    slidesPerView={1}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    >
+                    <SwiperSlide className="swiperSlide">
+                        <div class="serviceCard">
+                            <div class="content">
+                                <div class="title">Landingpage</div>
+                                <div class="price"><span className="ab">ab</span> 999€</div>
+                                <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at posuere eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.</div>
+                            </div>
+                            <a href="/anfragen" className="anfrageButton">
+                                Anfragen
+                                <i className="bi bi-arrow-up-right"></i>
+                            </a>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="swiperSlide">
+                        <div class="serviceCard">
+                            <div class="content">
+                                <div class="title">One Pager</div>
+                                <div class="price"><span className="ab">ab</span> 1999€</div>
+                                <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at posuere eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.</div>
+                            </div>
+                            <a href="/anfragen" className="anfrageButton">
+                                Anfragen
+                                <i className="bi bi-arrow-up-right"></i>
+                            </a>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="swiperSlide">
+                        <div class="serviceCard">
+                            <div class="content">
+                                <div class="title">Full Website</div>
+                                <div class="price"><span className="ab">ab</span> 4999€</div>
+                                <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at posuere eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.</div>
+                            </div>
+                            <a href="/anfragen" className="anfrageButton">
+                                Anfragen
+                                <i className="bi bi-arrow-up-right"></i>
+                            </a>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+            <span className="swipeText">
+                <i className="bi bi-chevron-double-left"></i>
+                Swipen
+                <i className="bi bi-chevron-double-right"></i>
+            </span>
             <div className="moreServiceBox">
                 <a href="/service" className="moreServiceButton">
                     Alle Services 
