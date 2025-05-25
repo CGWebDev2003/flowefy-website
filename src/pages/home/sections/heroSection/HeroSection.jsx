9a33d49c6d98ef8d47a5582680f6873ad84e82e0
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-05-25 16:04:34 +0200

    added atropos JS to Home Hero Section

--- src/pages/home/sections/heroSection/HeroSection.jsx
+++ src/pages/home/sections/heroSection/HeroSection.jsx
@@ -1,16 +1,27 @@
 import "./heroSection.css";
+import 'atropos/css';
+import Atropos from 'atropos/react';
 
 function HeroSection() {
-    return(
+    return (
         <div className="heroSection">
-            <h1 className="heroHeadline">Webdesign<br/>Agency</h1>
-            <h3 className="heroSubTitle">creative. individual. easy.</h3>
+            <Atropos shadow={false} activeOffset={40} className="my-atropos">
+                
+                <h1 className="heroHeadline" data-atropos-offset="3">
+                    Webdesign<br />Agency
+                </h1>
+                
+                <h3 className="heroSubTitle" data-atropos-offset="0">
+                    creative. individual. easy.
+                </h3>
+                
+                <div className="downArrows" data-atropos-offset="2">
+                    <i className="bi bi-chevron-down downArrow"></i>
+                    <i className="bi bi-chevron-down downArrow"></i>
+                    <i className="bi bi-chevron-down downArrow"></i>
+                </div>
 
-            <div className="downArrows">
-                <i className="bi bi-chevron-down downArrow"></i>
-                <i className="bi bi-chevron-down downArrow"></i>
-                <i className="bi bi-chevron-down downArrow"></i>
-            </div>
+            </Atropos>
         </div>
     );
 }
