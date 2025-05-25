a847de6940fa3f5c15d663ff16e271cf1d8abe2f
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-05-25 17:56:16 +0200

    Added images to home hero and fixed Header Background

--- src/pages/home/sections/heroSection/HeroSection.jsx
+++ src/pages/home/sections/heroSection/HeroSection.jsx
@@ -2,24 +2,68 @@ import "./heroSection.css";
 import 'atropos/css';
 import Atropos from 'atropos/react';
 
+import heroHanddrawn from "../../../../assets/hero/hero_handdrawn_arrows.png";
+import heroIconsSmall from "../../../../assets/hero/hero_icons_small.png";
+import heroIconsBig from "../../../../assets/hero/hero_icons_big.png";
+
 function HeroSection() {
     return (
         <div className="heroSection">
-            <Atropos shadow={false} activeOffset={40} className="my-atropos">
-                
-                <h1 className="heroHeadline" data-atropos-offset="3">
-                    Webdesign<br />Agency
-                </h1>
-                
-                <h3 className="heroSubTitle" data-atropos-offset="0">
-                    creative. individual. easy.
-                </h3>
-                
-                <div className="downArrows" data-atropos-offset="2">
-                    <i className="bi bi-chevron-down downArrow"></i>
-                    <i className="bi bi-chevron-down downArrow"></i>
-                    <i className="bi bi-chevron-down downArrow"></i>
+            <Atropos shadow={false} activeOffset={40} stretchZ={200} className="my-atropos">
+                {/* Ebene 5 – Arrows (Vordergrund) */}
+                <div
+                    className="atropos-layer heroAtroposBox handArrowBox"
+                    data-atropos-offset="5"
+                >
                 </div>
+                
+                {/* Ebene 3 – Headline (Vordergrund) */}
+                <div
+                    className="atropos-layer"
+                    data-atropos-offset="3"
+                >
+                    <h1 className="heroHeadline">
+                        Webdesign<br />Agency
+                    </h1>
+                </div>
+
+                {/* Ebene 5 – Arrows (Vordergrund) */}
+                <div
+                    className="atropos-layer heroAtroposBox smallIconBox"
+                    data-atropos-offset="2"
+                >
+                </div>
+
+                {/* Ebene 1 – Subheadline */}
+                <div
+                    className="atropos-layer"
+                    data-atropos-offset="0"
+                >
+                    <h3 className="heroSubTitle">
+                        creative. individual. easy.
+                    </h3>
+                </div>
+
+                {/* Ebene 5 – Arrows (Vordergrund) */}
+                <div
+                    className="atropos-layer heroAtroposBox bigIconBox"
+                    data-atropos-offset="1"
+                >
+                </div>
+
+                {/* Ebene 2 – Down Arrows */}
+                <div
+                    className="atropos-layer"
+                    data-atropos-offset="2"
+                >
+                    <div className="downArrows">
+                        <i className="bi bi-chevron-down downArrow"></i>
+                        <i className="bi bi-chevron-down downArrow"></i>
+                        <i className="bi bi-chevron-down downArrow"></i>
+                    </div>
+                </div>
+
+                
 
             </Atropos>
         </div>
