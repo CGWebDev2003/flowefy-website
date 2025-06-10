f045405f7ce08c44a2077da6cef37bd266eeb5f4
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-06-10 20:08:57 +0200

    added atropos to checkup section

--- src/pages/home/sections/checkupSection/CheckupSection.jsx
+++ src/pages/home/sections/checkupSection/CheckupSection.jsx
@@ -1,38 +1,41 @@
 import "./checkupSection.css";
-import 'atropos/css'
+import 'atropos/css';
+import Atropos from 'atropos/react';
 
 function CheckupSection() {
     return(
         <>
             <div className="checkupSection">
                 <div className="content checkupContent">
-                    <div className="checkupBox">
-                        <h1 className="checkupTitle headline">Dein Gratis Website Check</h1>
-                        <p className="checkupText">Lasse deine Website im kostenlosen flowefy-Website-Check testen und erfahre wo noch Luft nach oben ist. Dafür brauchst nicht mehr, als deine URL und eine Email. </p>
-                        <div className="checkupService">
-                            <p className="checkupItem">
-                                <i className="bi bi-check-circle"></i> 
-                                Design
-                            </p>
-                            <p className="checkupItem">
-                                <i className="bi bi-check-circle"></i> 
-                                Performance
-                            </p>
-                            <p className="checkupItem">
-                                <i className="bi bi-check-circle"></i> 
-                                Barrierefreiheit
-                            </p>
-                            <p className="checkupItem">
-                                <i className="bi bi-check-circle"></i> 
-                                Techn. Umsetzung
-                            </p>
-                            <p className="checkupItem">
-                                <i className="bi bi-check-circle"></i> 
-                                SEO 
-                            </p>
+                    <Atropos shadow={false} activeOffset={30} stretchZ={100} className="checkup-atropos">
+                        <div className="checkupBox atropos-layer" data-atropos-offset="0">
+                            <h1 className="checkupTitle headline atropos-layer" data-atropos-offset="2" >Dein Gratis Website Check</h1>
+                            <p className="checkupText atropos-layer" data-atropos-offset="1">Lasse deine Website im kostenlosen flowefy-Website-Check testen und erfahre wo noch Luft nach oben ist. Dafür brauchst nicht mehr, als deine URL und eine Email. </p>
+                            <div className="checkupService">
+                                <p className="checkupItem atropos-layer" data-atropos-offset="2">
+                                    <i className="bi bi-check-circle"></i> 
+                                    Design
+                                </p>
+                                <p className="checkupItem atropos-layer" data-atropos-offset="3">
+                                    <i className="bi bi-check-circle"></i> 
+                                    Performance
+                                </p>
+                                <p className="checkupItem atropos-layer" data-atropos-offset="3">
+                                    <i className="bi bi-check-circle"></i> 
+                                    Barrierefreiheit
+                                </p>
+                                <p className="checkupItem atropos-layer" data-atropos-offset="3">
+                                    <i className="bi bi-check-circle"></i> 
+                                    Techn. Umsetzung
+                                </p>
+                                <p className="checkupItem atropos-layer" data-atropos-offset="3">
+                                    <i className="bi bi-check-circle"></i> 
+                                    SEO 
+                                </p>
+                            </div>
+                            <a className="checkupButton  atropos-layer" data-atropos-offset="4" href="/checkup">Testen Lassen <i className="bi bi-arrow-up-right"></i></a>
                         </div>
-                        <a className="checkupButton" href="/checkup">Testen Lassen <i className="bi bi-arrow-up-right"></i></a>
-                    </div>
+                    </Atropos>
                 </div>
             </div>
         </>
