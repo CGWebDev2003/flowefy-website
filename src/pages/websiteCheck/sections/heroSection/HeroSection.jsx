e4b40b8c1a2e93834ea0e729fe560883743cffd2
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-06-27 02:17:19 +0200

    fixed desktop hero Section

--- src/pages/websiteCheck/sections/heroSection/HeroSection.jsx
+++ src/pages/websiteCheck/sections/heroSection/HeroSection.jsx
@@ -1,12 +1,19 @@
-import checklistImage from "../../../../assets/checkup/checklist.webp";
 import "./heroSection.css";
 
 function CheckHeroSection() {
     return(
         <div className="checkHeroSection">
             <div className="checkHeroSectionContent">
-                <h1 className="checkHeroTitle">Dein <span className="gratis">Gratis</span> Website <br/> Check</h1>
-                <img src={checklistImage} className="checklistImage" alt="Check-Liste" />
+                <span className="topPill"><i className="bi bi-check2-circle"></i> 100% kostenlos & unverbindlich</span>
+                <h1 className="checkHeroTitle">Kostenloser<br/>Website-Check</h1>
+                <p className="checkHeroText">Analysiere deine Website in <span className="highlighted bold">5 wichtigen Bereichen</span> und erhalte sofort professionelle Verbesserungsvorschläge</p>
+                <div className="checkHeroFeatures">
+                    <span className="heroFeature"><i className="bi bi-check2-circle checkFeatureIcon"></i> Design-Analyse</span>
+                    <span className="heroFeature"><i className="bi bi-check2-circle checkFeatureIcon"></i> Performance-Check</span>
+                    <span className="heroFeature"><i className="bi bi-check2-circle checkFeatureIcon"></i> SEO-Bewertung</span>
+                    <span className="heroFeature"><i className="bi bi-check2-circle checkFeatureIcon"></i> Mehr</span>
+                </div>
+                <button className="checkHeroCTAButton" type="button">Jetzt kostenlos prüfen <i class="bi bi-arrow-right"></i></button>
             </div>
         </div>
     );
