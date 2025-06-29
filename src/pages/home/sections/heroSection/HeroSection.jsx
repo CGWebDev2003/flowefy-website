b47f85896f0861ecb00ccfa41b13999934ac741b
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-06-30 01:23:37 +0200

    fixed home hero CTA button link

--- src/pages/home/sections/heroSection/HeroSection.jsx
+++ src/pages/home/sections/heroSection/HeroSection.jsx
@@ -16,7 +16,7 @@ function HeroSection() {
                 Wir denken uns in eure Marke und eure Kunden hinein – und gestalten Websites, die nicht nur gut aussehen, sondern funktionieren. Kreativ, durchdacht und mit dem Anspruch, echte Qualität abzuliefern.
                 <span className="highlighted bold">Schnell. Individuell. Bezahlbar.</span>
             </p>
-            <a href="https://tally.so/r/wk4EG1" target="_blank" className="heroCTAButton" type="button">Jetzt anfragen <i className="bi bi-arrow-up-right"></i></a>
+            <a href="#service" target="_blank" className="heroCTAButton" type="button">Usere Produkte <i className="bi bi-arrow-down"></i></a>
             <a href="#banner-section" className="heroDownButton"><i class="bi bi-mouse"></i></a>
         </div>
     );
