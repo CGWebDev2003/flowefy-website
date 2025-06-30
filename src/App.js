8af12fd1dfa701b81c31a174a58d83a6306ded64
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-06-30 04:40:04 +0200

    added bewerten page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import Anfragen from './pages/anfragen/Anfragen';
 import WebsiteCheck from './pages/websiteCheck/WebsiteCheck';
+import Bewerten from './pages/bewerten/Bewerten';
 import Impressum from './pages/impressum/Impressum';
 import Datenschutz from './pages/datenschutz/Datenschutz';
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Home />} /> 
         <Route path="/anfragen" element={<Anfragen />} />
         <Route path="/checkup" element={<WebsiteCheck />} />
+        <Route path="/bewerten" element={<Bewerten />} />
         <Route path="/impressum" element={<Impressum />} />
         <Route path="/datenschutz" element={<Datenschutz />} />
       </Routes>
---
+++ src/pages/bewerten/Bewerten.jsx
@@ -0,0 +1,11 @@
+import "./bewerten.css";
+
+function Bewerten() {
+    return(
+        <div className="bewertenSection">
+            <h1>Bewerten</h1>
+        </div>
+    );
+}
+
+export default Bewerten;
