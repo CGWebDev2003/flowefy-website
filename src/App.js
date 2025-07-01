5c8b461f5b5fc811552c2a3d543fb45f8d758d65
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-07-01 19:51:43 +0200

    setup basic UI and App Router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import Anfragen from './pages/anfragen/Anfragen';
 import WebsiteCheck from './pages/websiteCheck/WebsiteCheck';
+import Testen from './pages/websiteCheck/subpages/Testen';
 import Bewerten from './pages/bewerten/Bewerten';
 import Impressum from './pages/impressum/Impressum';
 import Datenschutz from './pages/datenschutz/Datenschutz';
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Home />} /> 
         <Route path="/anfragen" element={<Anfragen />} />
         <Route path="/checkup" element={<WebsiteCheck />} />
+        <Route path="/checkup/testen" element={<Testen />} />
         <Route path="/bewerten" element={<Bewerten />} />
         <Route path="/impressum" element={<Impressum />} />
         <Route path="/datenschutz" element={<Datenschutz />} />
